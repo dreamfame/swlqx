b05e43ab439983537e991189bca3ee96831c869a
贤凌 艾 <aixl@7itop.com>
2018-04-12 19:27:22 +0800

    补充提交

---
+++ Assets/Scripts/UIObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIObject : MonoBehaviour {
+
+
+    public GameObject M2P_Answer_Panel;
+
+    public void ShowM2PAnswerPanel() 
+    {
+        M2P_Answer_Panel.SetActive(true);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
